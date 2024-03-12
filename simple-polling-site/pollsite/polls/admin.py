d372f2bfa765db674b9c7fb142ff6b4f356fa35d
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2024-03-12 06:27:16 +0600

    added template

--- simple-polling-site/pollsite/polls/admin.py
+++ simple-polling-site/pollsite/polls/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Questions
+from .models import Questions, Choice
 
 # Register your models here.
 
 
 admin.site.register(Questions)
+admin.site.register(Choice)
 
--- simple-polling-site/pollsite/polls/views.py
+++ simple-polling-site/pollsite/polls/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Questions
 
 # Create your views here.
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." %question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
 
 def index(request):
-    return HttpResponse("This Is The poll site index.")
+    latest_question_list = Questions.objects.order_by("-published_date")[:5]
+    output = " , ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
